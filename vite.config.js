1706839e840dde3e2e5c4dc8c225eda9b66f28b8
chenchalasrinuui <66296488+chenchalasrinuui@users.noreply.github.com>
2026-02-08 15:33:54 +0530

    third commit

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true   // 👈 creates index.d.ts
+    })
+  ],
   build: {
     lib: {
-      entry: 'src/index.js',
-      name: 'VTSUILibrary',
-      fileName: 'vts-ui-lib',
+      entry: path.resolve(__dirname, "src/index.js"),
+      name: "VTSUILibrary",
+      fileName: (format) => `index.${format}.js`
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
-    },
-  },
-})
+      external: ["react", "react-dom"]
+    }
+  }
+});
